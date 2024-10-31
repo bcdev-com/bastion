a3618657bbc9380f109240fd2901eb0fa4251b20
Blake Coverett <blake@bcdev.com>
2024-10-30 21:12:45 -0700

    Nearly V1, needs rules text, possible layering

--- bastion.js
+++ bastion.js
@@ -1,18 +1,61 @@
-function DisplayLevel(layout, floorName, parentElement) {
-    let floor = null;
-    for(const f of layout.floors)
-        if (f.name === floorName) {
-            floor = f;
-            break;
-        }
-    if (!floor) return;
+// TODO: set cell sizes from layout?
+// Doors and windows, furnishings
+
+const layout = await (await fetch('./layout.json')).json();
+
+const roomDetails = document.getElementById('room-details');
+let selectedRoom = '';
+let clickedRoom = '';
+function SelectRoom(roomKey, click = false) {
+    if (click) {
+        if (clickedRoom === roomKey) roomKey = '';
+        clickedRoom = roomKey;
+    } else if (clickedRoom) {
+            roomKey = clickedRoom;
+    }
+    if (selectedRoom === roomKey) return;
+    if (selectedRoom) {
+        roomDetails.innerHTML = '';
+        for(const cell of layout.locations[selectedRoom].cells) 
+            cell.classList.remove('decoration-selected')
+        selectedRoom = '';
+    }
+    if (roomKey) {
+        const loc = layout.locations[roomKey];
+        const owner = loc.builder ? `<h2>Builder: ${loc.builder}<h2>` : '';
+        const r = document.getElementById('room-' + loc.type)
+        const rules = r ? r.innerHTML : `Missing Rules for Type ${loc.type}`;
+        roomDetails.innerHTML = 
+            `<h1>${loc.name}</h1>${owner}<p>${rules}</p>`;
+        for(const cell of loc.cells) 
+            cell.classList.add('decoration-selected');
+        selectedRoom = roomKey;
+    }
+}
+
+function SelectFloor(floorId) {
+    for(const f of layout.floors) {
+        document.getElementById(f.id).style.display = f.id === floorId ? 'block' : 'none';
+        document.getElementById(`show-${f.id}`).className = f.id === floorId ? 'active' : 'none';
+    }
+    SelectRoom('', true);
+}
+
+function RenderLevel(floor, bastionElement, navElement) {
     function GetKey(f, r, c) {
         if (r < 0 || r >= layout.height || c < 0 || c >= layout.width)
             return '\0';
         else
             return f.grid[r][c];
     }
-    let selectedKey = '';
+    const navButton = document.createElement('span');
+    navButton.id = `show-${floor.id}`;
+    navButton.innerText = floor.name;
+    navButton.addEventListener('click', e => SelectFloor(floor.id));
+    navElement.appendChild(navButton);
+    const parentElement = document.createElement('div');
+    parentElement.id = floor.id;
+    bastionElement.appendChild(parentElement);
     const table = document.createElement('table');
     table.classList.add('floorplan');
     parentElement.appendChild(table);
@@ -35,35 +78,23 @@ function DisplayLevel(layout, floorName, parentElement) {
             td.title = loc.name;
             td.style.backgroundColor = `color-mix(in srgb, ${loc.color}, white)`;
             td.style.borderColor = loc.color
-            if (GetKey(floor, row - 1, col) === key) td.style.borderTopStyle = 'dotted';
-            if (GetKey(floor, row, col + 1) === key) td.style.borderRightStyle = 'dotted';
-            if (GetKey(floor, row + 1, col) === key) td.style.borderBottomStyle = 'dotted';
-            if (GetKey(floor, row, col - 1) === key) td.style.borderLeftStyle = 'dotted';
-            const f = e => {
-                if (selectedKey !== key) {
-                    const room = document.getElementById('room');
-                    if (selectedKey) {
-                        room.innerHTML = '';
-                        for(const cell of layout.locations[selectedKey].cells) cell.innerText = '';
-                    }
-                    const owner = loc.owner ? `<h2>Builder: ${loc.owner}<h2>` : '';
-                    const r = document.getElementById('room-' + loc.type)
-                    const rules = r ? r.innerHTML : `Missing Rules for Type ${loc.type}`;
-                    room.innerHTML = 
-                        `<h1>${loc.name}</h1>${owner}<p>${rules}</p>`;
-                    for(const cell of loc.cells) cell.innerText = '•';
-                    selectedKey = key;
-                }
-            };
-            td.addEventListener('tap', f);
-            td.addEventListener('mouseenter', f);
+            if (GetKey(floor, row - 1, col) === key) 
+                td.style.borderTopStyle = 'dotted';
+            if (GetKey(floor, row, col + 1) === key) 
+                td.style.borderRightStyle = 'dotted';
+            if (GetKey(floor, row + 1, col) === key) 
+                td.style.borderBottomStyle = 'dotted';
+            if (GetKey(floor, row, col - 1) === key) 
+                td.style.borderLeftStyle = 'dotted';
+            td.addEventListener('click', () => SelectRoom(key, true));
+            td.addEventListener('mouseenter', () => SelectRoom(key));
+            td.addEventListener('mouseleave', () => SelectRoom(''));
             tr.appendChild(td);
         }
     }
 }
 
-const layout = await (await fetch('./layout.json')).json();
-DisplayLevel(layout, 'Upper', document.getElementById('upper'));
-DisplayLevel(layout, 'Ground', document.getElementById('ground'));
-DisplayLevel(layout, 'Basement', document.getElementById('basement'));
-if (document.location.hash === '') document.location.hash = '#ground';
+const b = document.getElementById('bastion');
+const n = document.getElementById('level-select');
+for(const f of layout.floors) RenderLevel(f, b, n);
+SelectFloor(layout.defaultFloor);
